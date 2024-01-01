bd151dbb71f388010b6b243e74c572d28deae1b8
Ognjen Mišić <omg.gio@gmail.com>
2024-01-01 12:48:10 +0100

    rustlings wip

--- exercises/options/options1.rs
+++ exercises/options/options1.rs
@@ -3,8 +3,6 @@
 // Execute `rustlings hint options1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 // This function returns how much icecream there is left in the fridge.
 // If it's before 10PM, there's 5 pieces left. At 10PM, someone eats them
 // all, so there'll be no more left :(
@@ -13,7 +11,20 @@ fn maybe_icecream(time_of_day: u16) -> Option<u16> {
     // value of 0 The Option output should gracefully handle cases where
     // time_of_day > 23.
     // TODO: Complete the function body - remember to return an Option!
-    ???
+
+    match time_of_day {
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        _ => None,
+    }
+
+    // if time_of_day > 23 {
+    //     None
+    // } else if time_of_day > 21 {
+    //     Some(0)
+    // } else {
+    //     Some(5)
+    // }
 }
 
 #[cfg(test)]
@@ -22,10 +33,12 @@ mod tests {
 
     #[test]
     fn check_icecream() {
+        assert_eq!(maybe_icecream(0), Some(5));
         assert_eq!(maybe_icecream(9), Some(5));
         assert_eq!(maybe_icecream(10), Some(5));
-        assert_eq!(maybe_icecream(23), Some(0));
+        assert_eq!(maybe_icecream(21), Some(5));
         assert_eq!(maybe_icecream(22), Some(0));
+        assert_eq!(maybe_icecream(23), Some(0));
         assert_eq!(maybe_icecream(25), None);
     }
 
@@ -34,6 +47,6 @@ mod tests {
         // TODO: Fix this test. How do you get at the value contained in the
         // Option?
         let icecreams = maybe_icecream(12);
-        assert_eq!(icecreams, 5);
+        assert_eq!(icecreams.unwrap_or(0), 5);
     }
 }
