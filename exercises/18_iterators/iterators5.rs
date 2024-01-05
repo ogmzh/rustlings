abf4b17803f19edf4fb24207799dadbab2bb7f90
ogmzh <omg.gio@gmail.com>
2024-01-05 14:27:56 +0100

    iterators

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -11,8 +11,6 @@
 // Execute `rustlings hint iterators5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -35,7 +33,11 @@ fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    // todo!();
+
+    // if map has value get ++
+    // map.values().filter(|x| x == value).count() // no implementation for `&&Progress == Progress` (double reference because 1. map (fn argument) is a reference, and then filter closure also passes a reference)
+    map.values().filter(|&x| x == &value).count()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
@@ -54,7 +56,9 @@ fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Pr
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    // todo!();
+    // collectionforeach or map?
+    collection.iter().map(|x| count_iterator(&x, value)).sum()
 }
 
 #[cfg(test)]
